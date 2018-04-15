a5f323addd446bc0c02c1a6bbeb5663294521051
jimmy <505487263@qq.com>
2018-04-15 20:46:10 +0800

    first commit

---
+++ client.php
@@ -0,0 +1,31 @@
+<?php  
+  
+$config = array(  
+            'location' => 'http://localhost/webservice/service.php',  
+            'uri' => 'myserver',  
+            'login' => 'fdipzone',  
+            'password' => '123456',  
+            'trace' => true  
+);  
+  
+try{  
+  
+    $auth = array('fdipzone', '654321');  
+  
+    // no wsdl  
+    $client = new SOAPClient(null, $config);  
+    $header = new SOAPHeader('myserver', 'auth', $auth, false, SOAP_ACTOR_NEXT);  
+    $client->__setSoapHeaders(array($header));  
+  
+    $revstring = $client->revstring('123456');  
+    $strtolink = $client->__soapCall('strtolink', array('http://blog.csdn.net/fdipzone', 'fdipzone blog', 1));  
+    $uppcase = $client->__soapCall('uppcase', array('Hello World'));  
+  
+    echo $revstring.'<br>';  
+    echo $strtolink.'<br>';  
+    echo $uppcase.'<br>';  
+  
+}catch(SOAPFault $e){  
+    echo $e->getMessage();  
+}  
+  
---
+++ service.php
@@ -0,0 +1,57 @@
+<?php  
+  
+// 服务器验证    
+/*if ($_SERVER['PHP_AUTH_USER']!='fdipzone' || $_SERVER['PHP_AUTH_PW']!='123456') {  
+    header('WWW-Authenticate: Basic realm="NMG Terry"');  
+    header('HTTP/1.0 401 Unauthorized');  
+    echo "You must enter a valid login ID and password to access this resource.\n";  
+    exit();  
+}  */
+  
+class SOAPHandle{ // class start  
+  
+    // header 驗證  
+    public function auth($auth){  
+        if($auth->string[0]!='fdipzone' || $auth->string[1]!='654321'){  
+            throw new SOAPFault('Server', 'No Permission');  
+        }  
+    }  
+  
+    // 反轉字符串  
+    public function revstring($str=''){  
+        return strrev($str);  
+    }  
+  
+    // 字符傳轉連接  
+    public function strtolink($str='', $name='', $openwin=0){  
+        $name = $name==''? $str : $name;  
+        $openwin_tag = $openwin==1? ' target="_blank" ' : '';  
+        return sprintf('<a href="%s" %s>%s</a>', $str, $openwin_tag, $name);  
+    }  
+  
+    // 字符串轉大寫  
+    public function uppcase($str){  
+        return strtoupper($str);  
+    }  
+  
+  
+} // class end  
+  
+$config = array(  
+    'uri' => 'myserver'  
+);  
+  
+$oHandle = new SOAPHandle;  
+  
+// no wsdl mode  
+try{  
+  
+    $server = new SOAPServer(null, $config);  
+    $server->setObject($oHandle);  
+    $server->handle();  
+  
+}catch(SOAPFault $f){  
+  
+    echo $f->faultString;  
+  
+}
